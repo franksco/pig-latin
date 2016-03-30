ccdc843c138f8cef8f6bdfbf6d08f0197304356f
Cory Franks and Jake Richmond <cory@>
2016-03-30 14:00:40 -0700

    get it working for one word

---
+++ js/script.js
@@ -0,0 +1,50 @@
+/// User Interface Logic ///
+$(document).ready(function() {
+  $("form.pig-latin").submit(function(event) {
+    var word = ($("input#word").val());
+    var new_word = wordPigLatin(word);
+    console.log(word);
+    $(".new_word").text(new_word);
+    console.log("we here?")
+    $("#result").show();
+    event.preventDefault();
+
+  });
+});
+
+/// Business Logic ///
+function sentencePigLatin(sentence) {
+  var silly_sentence = [];
+  var silly_word = "";
+  console.log("do da doo");
+
+  var new_sentence = sentence.split(" ");
+  new_sentence.forEach(function(item) {
+    silly_word = wordPigLatin(item);
+    silly_sentence.push(silly_word);
+  });
+    return silly_sentence.join(' ');
+};
+
+function wordPigLatin(word) {
+  var vowels = ["a", "e", "i", "o", "u"];
+  console.log("test");
+
+  var first_letter = word.charAt(0);
+  var second_letter = word.charAt(1);
+
+  if (vowels.indexOf(first_letter) === 0) {
+    var word = word.concat("ay");
+    return word;
+
+  } else if ((vowels.indexOf(first_letter) === -1) && (vowels.indexOf(second_letter) === -1)) {
+    var two_letters = word.slice(0, 2);
+    var new_word = word.substr(2).concat(two_letters).concat("ay");
+    return new_word;
+
+  } else if (vowels.indexOf(first_letter) === -1) {
+    var letter = word.slice(0, 1);
+    var new_word = word.substr(1).concat(letter).concat("ay");
+    return new_word;
+  }
+};
