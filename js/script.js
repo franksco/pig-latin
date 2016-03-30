c5010804539286fd49b12fdc895b8396154152ab
Cory Franks and Jake Richmond <cory@>
2016-03-30 15:41:53 -0700

    final working process for both word and sentence

--- js/script.js
+++ js/script.js
@@ -3,24 +3,23 @@ $(document).ready(function() {
   $("form.pig-latin").submit(function(event) {
     var word = ($("input#word").val());
     var new_word = wordPigLatin(word);
-    console.log(word);
+    var sentence = ($("input#sentence").val());
+    var new_sentence = sentencePigLatin(sentence);
     $(".new_word").text(new_word);
-    console.log("we here?")
+    $(".new_sentence").text(new_sentence);
     $("#result").show();
     event.preventDefault();
 
   });
 });
 
-/// Business Logic ///
+///Business Logic ///
 function sentencePigLatin(sentence) {
   var silly_sentence = [];
-  var silly_word = "";
-  console.log("do da doo");
 
   var new_sentence = sentence.split(" ");
   new_sentence.forEach(function(item) {
-    silly_word = wordPigLatin(item);
+    var silly_word = wordPigLatin(item);
     silly_sentence.push(silly_word);
   });
     return silly_sentence.join(' ');
@@ -33,11 +32,11 @@ function wordPigLatin(word) {
   var first_letter = word.charAt(0);
   var second_letter = word.charAt(1);
 
-  if (vowels.indexOf(first_letter) === 0) {
-    var word = word.concat("ay");
+  if (first_letter === vowels[0] || first_letter === vowels[1] || first_letter === vowels[2] || first_letter === vowels[3] || first_letter === vowels[4]) {
+    var word = word.concat("way");
     return word;
 
-  } else if ((vowels.indexOf(first_letter) === -1) && (vowels.indexOf(second_letter) === -1)) {
+  } else if (vowels.indexOf(first_letter) === -1 && vowels.indexOf(second_letter) === -1) {
     var two_letters = word.slice(0, 2);
     var new_word = word.substr(2).concat(two_letters).concat("ay");
     return new_word;
